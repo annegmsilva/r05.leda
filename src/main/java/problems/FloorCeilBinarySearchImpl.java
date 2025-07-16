f8c151f4b154e0cc4590b2acf8917e24eada1382
124110731 ANNE GRAZIELI MARQUES SILVA <anne.grazieli.marques.silva@lcc3-003.lcc.ufcg.edu.br>
2025-07-16 09:47:48 -0300

    ceilfloor

--- src/main/java/problems/FloorCeilBinarySearchImpl.java
+++ src/main/java/problems/FloorCeilBinarySearchImpl.java
@@ -17,35 +17,42 @@ public class FloorCeilBinarySearchImpl implements FloorCeil {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		Integer middleIndex = array.length/2;
 		Integer floor = null;
-		if (x == array[middleIndex]) {
-			Integer[] left = Arrays.copyOfRange(array, 0, middleIndex);
-			floor = floor(left,x);
-		} else if (x < array[middleIndex]) {
-			Integer[] left = Arrays.copyOfRange(array, 0, middleIndex);
-			floor = floor(left,x);
-		} else {
-			Integer[] right = Arrays.copyOfRange(array, middleIndex, array.length);
-			floor = floor(right,x);
+		if (array.length > 0) {
+			Integer middleIndex = array.length/2;
+			if (x == array[middleIndex]) {
+				floor = array[middleIndex];
+			} else if (x < array[middleIndex]) {
+				Integer[] left = Arrays.copyOfRange(array, 0, middleIndex);
+				floor = floor(left,x);
+			} else {
+				Integer[] right = Arrays.copyOfRange(array, middleIndex + 1, array.length);
+				floor = floor(right,x);
+				if (floor == null) {
+					floor = array[middleIndex];
+				}
+			}
 		}
 		return floor;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
-		Integer middleIndex = array.length/2;
 		Integer ceil = null;
-		
-		if (x == array[middleIndex]) {
-			Integer[] right = Arrays.copyOfRange(array, middleIndex, array.length);
-			ceil = ceil(right,x);
-		} else if (x < array[middleIndex]) {
-			Integer[] right = Arrays.copyOfRange(array, middleIndex, array.length);
-			ceil = ceil(right,x);
-		} else {
-			Integer[] left = Arrays.copyOfRange(array, 0, middleIndex);
-			ceil = ceil(left,x);
+		if (array.length > 0) {
+			Integer middleIndex = array.length/2;
+			if (x == array[middleIndex]) {
+				ceil = array[middleIndex];
+			} else if (x > array[middleIndex]) {
+				Integer[] right = Arrays.copyOfRange(array, middleIndex + 1, array.length);
+				ceil = ceil(right,x);
+			} else {
+				Integer[] left = Arrays.copyOfRange(array, 0, middleIndex);
+				ceil = ceil(left,x);
+				if (ceil == null) {
+					ceil = array[middleIndex];
+				} 
+			}
 		}
 		return ceil;
 	}
